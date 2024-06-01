3f76898b9ed35b675c64b0c25f30501bb497cde4
KavyaPremaseelan <kavyapremaseelan@gmail.com>
2024-06-01 20:06:17 +0530

    second commit

---
+++ SortingAlgorithms/Bubblesort.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Bubblesort {
+    public static void bubble(int arr[], int n) {
+        for (int i = n - 1; i >= 1; i--) {
+            int didSwap = 0;
+            for (int j = 0; j <= i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    didSwap = 1;
+                }
+            }
+            // optimization
+            if (didSwap == 0) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements : ");
+        int n = sc.nextInt();
+        System.out.println("Enter the elements of the array");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        bubble(arr, n);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+}
